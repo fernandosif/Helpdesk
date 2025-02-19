99e59fefacfa0c5a7f28230041b474d26e6e97ef
fernandosif <fernando.mibr@gmail.com>
2025-02-18 21:51:00 -0300

    projeto iniciado

---
+++ src/test/java/com/fnd/helpdesk1/Helpdesk1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fnd.helpdesk1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Helpdesk1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
